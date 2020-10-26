1fb0b854e0113138280a54eb55032c17e519549d
ro2306 <robahhhhh@gmail.com>
2020-10-26 18:48:32 +0200

    excercise 1 done

---
+++ weke-1/Variables & Operators/ex1.js
@@ -0,0 +1,61 @@
+// Exercise 1
+
+const maxCapacity = 14
+let surfTime = true
+let bestStudent = 13
+const purposeInLife=5
+
+ console.log(maxCapacity)
+ console.log(surfTime)
+ console.log(bestStudent)
+ console.log(purposeInLife)
+
+ // Exercise 2
+
+ const myVariable = "Let's try this "
+	
+const result = myVariable + "is the best thing ever"
+console.log(result)
+
+// Exercise 3
+
+const password = "badO23"
+const confirmPassword = "bad023"
+
+console.log(password == confirmPassword)
+
+// Exercise 4
+console.log(423*12)
+console.log(802/2)
+console.log((5+6)*3)
+
+// Exercise 5
+
+console.log((5 > 2) && false) // false
+
+console.log("knife" === "sword") // true
+
+console.log((1 < 2) || (-1 > -1) || !false) // true
+
+console.log("") // false
+
+console.log((31 % 5) == "1") // true﻿
+
+console.log(!!true) //true﻿
+
+console.log("5th Avenue" != "5th Avenue") // false﻿
+
+console.log(52 !== "52") //true﻿
+
+console.log(undefined || null) // null﻿
+
+// Exercise 5
+let a = 3
+a = 4
+let c = 0
+let b = a
+b = 2
+a = b
+console.log (b = c)
+console.log (c = a)
+console.log (a = b)
